b22460d9c2b920000a521ffcc34632dc2c14e6ff
creamsandwch <neuronrus1996@gmail.com>
2023-07-21 10:55:31 +0500

    feat: added custom user model w manager and unittest for creating user & superuser

---
+++ users/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+from users.managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    """
+    Кастомная модель пользователя.
+    """
+    email = models.EmailField(
+        verbose_name=_('адрес электронной почты'),
+        unique=True
+    )
+    first_name = models.CharField(
+        blank=True, max_length=150, verbose_name=_('first name')
+    )
+    last_name = models.CharField(
+        blank=True, max_length=150, verbose_name=_('last name')
+    )
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('дата регистрации')
+    )
+
+    USERNAME_FIELD = 'email'
+
+    # для требуемых для заполнения полей в промптах
+    # при создании суперпользователя через консоль
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
