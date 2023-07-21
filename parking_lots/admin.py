1787d2867f522f69fd3592213a200a6935f14753
mxstrv <37843972+mxstrv@users.noreply.github.com>
2023-07-21 11:30:21 +0300

    feat: add parking lot admin model

--- parking_lots/admin.py
+++ parking_lots/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ParkingLot
+
+
+class ParkingLotAdmin(admin.ModelAdmin):
+    """
+    Черновая версия админки парковки.
+    """
+    list_display = [
+        'address',
+        'coordinates',
+        'car_capacity'
+    ]
+    search_fields = ['address', ]
+
+
+admin.site.register(ParkingLot, ParkingLotAdmin)
