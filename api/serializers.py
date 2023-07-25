7ba8601f5ddeb1d80b3af4f3bdfbe124a202857e
creamsandwch <neuronrus1996@gmail.com>
2023-07-25 10:04:01 +0500

    init api + djoser verification

---
+++ api/serializers.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import authenticate, get_user_model
+from django.shortcuts import get_object_or_404
+from django.utils.translation import gettext_lazy as _
+
+from djoser.conf import settings as djoser_settings
+from djoser.serializers import TokenCreateSerializer
+
+
+User = get_user_model()
+
+
+class CustomTokenCreateSerializer(TokenCreateSerializer):
+    def validate(self, data):
+        password = data.get('password')
+        params = {
+            djoser_settings.LOGIN_FIELD: data.get(djoser_settings.LOGIN_FIELD)
+        }
+        self.user = authenticate(
+            request=self.context.get('request'),
+            **params,
+            password=password
+        )
+        if not self.user:
+            self.user = get_object_or_404(User, **params)
+            if self.user and not self.user.check_password(password):
+                self.fail(_('Неверные данные'))
+
+        if self.user:
+            return data
+        self.fail(_('Неверные данные'))
