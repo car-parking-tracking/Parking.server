4f69b7cfe45ab4e04a40e08242d81d27d54da9f9
Лев Андреев <leoohard@yandex.ru>
2023-08-11 21:44:18 +0300

    fix endpoints

--- api/serializers.py
+++ api/serializers.py
@@ -20,8 +20,8 @@ class ParkingLotSerializer(serializers.ModelSerializer):
         fields = [
             'id',
             'address',
-            'coordinates_x',
-            'coordinates_y',
+            'latitude',
+            'longitude',
             'car_capacity',
             'tariffs',
             'is_favorited'
@@ -74,7 +74,7 @@ class FeatureSerializer(serializers.ModelSerializer):
     def get_geometry(self, obj: ParkingLot) -> dict:
         return {
             'type': 'Point',
-            'coordinates': [obj.coordinates_x, obj.coordinates_y]
+            'coordinates': [obj.latitude, obj.longitude]
         }
 
     def get_properties(self, obj: ParkingLot) -> dict:
