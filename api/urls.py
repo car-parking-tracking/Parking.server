79740149dfe4220499723ac3080505815c139b6c
Лев Андреев <leoohard@yandex.ru>
2023-08-14 16:04:22 +0300

    change simplerouter on defaultrouter

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,16 @@
 from django.urls import include, path
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 
 from .views import ParkingLotViewSet, FeaturesViewSet
 
-router = SimpleRouter()
-router.register('parking_lots', ParkingLotViewSet, basename='parkinglots')
+router = DefaultRouter()
+router.register(r'parking_lots', ParkingLotViewSet, basename='parkinglots')
 router.register(
-    'feature_collection', FeaturesViewSet, basename='feature_collection'
+    r'feature_collection', FeaturesViewSet, basename='feature_collection'
 )
 
 urlpatterns = [
-    path('', include('djoser.urls')),
     path('', include(router.urls)),
+    path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
