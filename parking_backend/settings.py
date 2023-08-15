7ae5e84da571f4ab3f291e8a95cddf8fad75045d
Лев Андреев <leoohard@yandex.ru>
2023-08-15 14:51:18 +0300

    fix cors mistake

--- parking_backend/settings.py
+++ parking_backend/settings.py
@@ -30,6 +30,10 @@ CSRF_TRUSTED_ORIGINS = [
     'https://' + str(os.getenv('HOST_ADDRESS')),
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+
 # DJOSER SETTINGS
 DJOSER = {
     "LOGIN FIELD": "email",
@@ -64,6 +68,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -160,3 +165,6 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
