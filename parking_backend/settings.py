37be9c0f56ba86d78eb2222bad3dada7aaa36579
Лев Андреев <leoohard@yandex.ru>
2023-08-26 22:20:55 +0300

    change env.example delete ENGINE

--- parking_backend/settings.py
+++ parking_backend/settings.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = 'parking_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
