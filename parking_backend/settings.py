5440d208e4cbcb0705c4cdd9c70aa400921d4f7c
Лев Андреев <leoohard@yandex.ru>
2023-08-27 15:10:25 +0300

    change paginations pages 10

--- parking_backend/settings.py
+++ parking_backend/settings.py
@@ -80,7 +80,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-        'PAGE_SIZE': 100,
+        'PAGE_SIZE': 10,
 }
 
 ROOT_URLCONF = 'parking_backend.urls'
