52c1d39d1e7f285dee9030dfa16f4f8d2f07fa1e
Артём Куркин <artykn88@gmail.com>
2023-11-27 12:00:31 +0300

    added pagination in the admin model and some fixes users

--- users/apps.py
+++ users/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
+    verbose_name = 'Пользователи'
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
 
